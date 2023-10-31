445a1c13ca680d38eeec69de848b4c297d2cf753
Samuel Holland <samuel@sholland.org>
2023-10-31 03:40:19 -0500

    regulator: Don't fail when already enabled
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- drivers/power/regulator/regulator_common.c
+++ drivers/power/regulator/regulator_common.c
@@ -76,7 +76,7 @@ int regulator_common_set_enable(const struct udevice *dev,
 	/* If previously enabled, increase count */
 	if (enable && plat->enable_count > 0) {
 		plat->enable_count++;
-		return -EALREADY;
+		return 0;
 	}
 
 	if (!enable) {
@@ -86,7 +86,7 @@ int regulator_common_set_enable(const struct udevice *dev,
 			return -EBUSY;
 		} else if (!plat->enable_count) {
 			/* If already disabled, do nothing */
-			return -EALREADY;
+			return 0;
 		}
 	}
 
